29baf0209e2517f96071649ef513c15a6e32d0f4
Oriol Torrent Florensa <oriol.torrent@unic.com>
2019-02-21 15:02:52 +0100

    Set NVM version as a variable

--- configs/installation.sh
+++ configs/installation.sh
@@ -1,11 +1,16 @@
 # Folders
-bin_dir="/usr/local/bin"
 work_dir="$HOME/Sites/"
 tools_dir="$HOME/Sites/_Tools"
 nvm_dir="$HOME/.nvm"
+
+bin_dir="/usr/local/bin"
 
 # Set desired terminal theme name
 # https://github.com/lysyi3m/macos-terminal-themes.git
 TERMINAL_THEME="FrontEndDelight"
 
+# Set the desired text to be displayed in Login window
 LOGIN_WINDOW_TEXT="This computer belongs to Unic AG, please call +41 44 560 12 12"
+
+# Set the latest NVM version by checking https://github.com/creationix/nvm/releases
+nvm_latest_version="0.34.0"
